2a643bc89b27ee6a6fa2430fba2830c502997b10
Valerii <kulibchenkovalera@gmail.com>
2023-08-22 21:10:17 +0300

    add CustomGlobalExceptionHandler and validation Dto

--- src/main/java/com/example/springboot/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/springboot/dto/CreateBookRequestDto.java
@@ -1,13 +1,26 @@
 package com.example.springboot.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
+    @Size(min = 2)
     private String title;
+    @NotNull
+    @Size(min = 2)
     private String author;
+    @NotNull
+    @Min(value = 0)
     private String isbn;
+    @NotNull
+    @Min(value = 0)
     private String price;
+    @NotNull
     private String description;
+    @NotNull
     private String coverImage;
 }
