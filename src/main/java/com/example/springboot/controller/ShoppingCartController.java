7a34021fa74136a5b055fbdb9bd3f8a5a3cf1581
kulib <kulibchenkovalera@gmail.com>
2023-10-09 22:02:43 +0300

    third commit

--- src/main/java/com/example/springboot/controller/ShoppingCartController.java
+++ src/main/java/com/example/springboot/controller/ShoppingCartController.java
@@ -13,7 +13,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
